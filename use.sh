5f471f74d27d453e83257f0b2d6a161806887b4a
Joseph J. Bautista <joseph.bautista@proton.me>
2024-12-11 20:32:47 +0100

    feat: added npm install in use.sh script

--- use.sh
+++ use.sh
@@ -16,6 +16,7 @@ main() {
   fi
 
   SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+  npm install
   git config --global alias.gen-commit "!${SCRIPT_DIR}/run.sh"
   echo "Successfully added 'gen-commit' as a Git alias"
 }
